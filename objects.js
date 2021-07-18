533854d4a95ad7e3576fb94e2773257fde17b08d
maarjaroots <maarja.roots@gmail.com>
2021-07-18 09:27:08 +0100

    intro for objects

---
+++ objects.js
@@ -0,0 +1,4 @@
+// An object is an unordered collection of properties, each of which has a name and a value. In adddition to maintaining its own set of properties,
+// a JS object also inheritd the properties of another object, known as its "prototype".
+//The methods of an object are typically inherited properties, and this 'prototypal inheritance is key feature in JS
+// Object are mutable
